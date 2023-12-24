bbcbbb33be0f396ff79db88853265399a34f6bc8
priyanshu <agrawal.priyanshu51@gmail.com>
2023-12-24 12:28:47 +0530

    Added upto dynamic programming

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,9 @@
-import ch_2.InsertionSort;
-import ch_2.MergeSort;
-import ch_4.MaxSubarray;
-import ch_6.HeapSort;
-import ch_7.QuickSort;
+import ch_14.matrix_chain.MatrixChainBottomUp;
+import ch_14.matrix_chain.MatrixChainTopDown;
 
 public class Main {
     public static void main(String[] args) {
-        QuickSort.quickSortDriverCode();
+        MatrixChainBottomUp.matrixChainRecursiveDriverCode();
+        MatrixChainTopDown.matrixChainTopDownDriverCode();
     }
 }
